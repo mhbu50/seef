5cd4b1b13eb2510f5e6f88d58687485e39547686
root <root@instance-1.us-east1-b.c.zeta-bonsai-229314.internal>
2019-03-05 11:58:30 +0000

    add custom

---
+++ seef/seef_app/doctype/accreditation_exchange/accreditation_exchange.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Accurate Systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Accreditation Exchange', {
+	refresh: function(frm) {
+
+	}
+});
